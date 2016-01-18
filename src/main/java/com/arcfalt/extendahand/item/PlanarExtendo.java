645bc246dfd8f84e25e28c5ff983bcb1a45e61ad
Arcfalt <me@arcfalt.com>
2016-01-18 22:57:26 +1000

    Box selection

--- src/main/java/com/arcfalt/extendahand/item/PlanarExtendo.java
+++ src/main/java/com/arcfalt/extendahand/item/PlanarExtendo.java
@@ -1,12 +1,18 @@
 package com.arcfalt.extendahand.item;
 
+import com.arcfalt.extendahand.utils.ItemUtils;
+import com.arcfalt.extendahand.utils.RenderUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3i;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -32,18 +38,39 @@ public class PlanarExtendo extends BasePointExtendo
 		//ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
 	}
 
-	/*
 	@Override
 	protected Set<BlockPos> actingBlocks(BlockPos blockPos, EnumFacing sideHit, World world, EntityPlayer player)
 	{
 		Set<BlockPos> positions = new HashSet<BlockPos>();
-		BlockPos offsetPos = blockPos.offset(sideHit);
 
-		IBlockState blockState = world.getBlockState(offsetPos);
-		Block block = blockState.getBlock();
+		ItemStack stack = player.getHeldItem();
+		NBTTagCompound tags = ItemUtils.getOrCreateTagCompound(stack);
+		String posTag0 = LOC + 0;
+		String posTag1 = LOC + 1;
+		if(!tags.hasKey(posTag0) || !tags.hasKey(posTag1)) return positions;
 
-		if(block.getMaterial() == Material.air) positions.add(offsetPos);
+		BlockPos locA = BlockPos.fromLong(tags.getLong(posTag0));
+		BlockPos locB = BlockPos.fromLong(tags.getLong(posTag1));
+		Vec3i minLoc = new Vec3i(Math.min(locA.getX(), locB.getX()), Math.min(locA.getY(), locB.getY()), Math.min(locA.getZ(), locB.getZ()));
+		Vec3i maxLoc = new Vec3i(Math.max(locA.getX(), locB.getX()), Math.max(locA.getY(), locB.getY()), Math.max(locA.getZ(), locB.getZ()));
+
+		// Todo - Optimize this, currently using an inefficient algorithm because I made it while tired
+		for(int x = minLoc.getX(); x <= maxLoc.getX(); x++)
+		{
+			for(int y = minLoc.getY(); y <= maxLoc.getY(); y++)
+			{
+				for(int z = minLoc.getZ(); z <= maxLoc.getZ(); z++)
+				{
+					if(x == minLoc.getX() || x == maxLoc.getX() || y == minLoc.getY() || y == maxLoc.getY() || z == minLoc.getZ() || z == maxLoc.getZ())
+					{
+						BlockPos acting = new BlockPos(x, y, z);
+						IBlockState blockState = world.getBlockState(acting);
+						Block block = blockState.getBlock();
+						if(block.getMaterial() == Material.air) positions.add(acting);
+					}
+				}
+			}
+		}
 		return positions;
 	}
-	*/
 }

cd94b74d749c79b0a150b3b6bd6c92b4cf8222fd
Arcfalt <me@arcfalt.com>
2016-07-09 16:02:43 +1000

    Updated mod version info

--- src/main/java/com/arcfalt/extendahand/ExtendAHand.java
+++ src/main/java/com/arcfalt/extendahand/ExtendAHand.java
@@ -15,7 +15,7 @@ public class ExtendAHand
 
 	public static final String MODID = "ExtendAHand";
 	public static final String MODNAME = "Extend-A-Hand";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.2.1";
 
 	@SidedProxy(clientSide = "com.arcfalt.extendahand.proxy.ClientProxy", serverSide = "com.arcfalt.extendahand.proxy.ServerProxy")
 	public static CommonProxy proxy;

29e88c2b92390f98058563c39855d98dd8185c0f
Arcfalt <me@arcfalt.com>
2016-07-09 14:12:32 +1000

    Removed beta tag from mod version

--- src/main/java/com/arcfalt/extendahand/ExtendAHand.java
+++ src/main/java/com/arcfalt/extendahand/ExtendAHand.java
@@ -15,7 +15,7 @@ public class ExtendAHand
 
 	public static final String MODID = "ExtendAHand";
 	public static final String MODNAME = "Extend-A-Hand";
-	public static final String VERSION = "1.1b";
+	public static final String VERSION = "1.1";
 
 	@SidedProxy(clientSide = "com.arcfalt.extendahand.proxy.ClientProxy", serverSide = "com.arcfalt.extendahand.proxy.ServerProxy")
 	public static CommonProxy proxy;
